6a8a48c8877e59bacf58d0412ae96912d496740a
dkswoals <a4445656@gmail.com>
2024-09-20 18:23:27 +0900

    Feat: Board, Tile 엔티티 구현

---
+++ Server/src/main/java/com/example/server/Entity/Board.java
@@ -0,0 +1,66 @@
+package com.example.server.Entity;
+
+import static com.example.server.constant.Constant.BOARD_CYCLE;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @Column(name = "member_position")
+    private Integer memberPosition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tile> tiles = new ArrayList<>();
+
+    public Board(Long id, Integer memberPosition, Member member, List<Tile> tiles) {
+        this.id = id;
+        this.memberPosition = memberPosition;
+        this.member = member;
+        this.tiles = tiles;
+    }
+
+    public Board() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getMemberPosition() {
+        return memberPosition;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public List<Tile> getTiles() {
+        return tiles;
+    }
+
+    public void moveMemberPosition(Integer dice) {
+        this.memberPosition = (this.memberPosition + dice) % BOARD_CYCLE;
+    }
+
+}
---
+++ Server/src/main/java/com/example/server/Entity/Tile.java
@@ -0,0 +1,72 @@
+package com.example.server.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Tile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tile_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+
+    @Column(name = "order")
+    private Integer order;
+
+    @Column(name = "state")
+    private Boolean state;
+
+    public Tile(Long id, Board board, Mission mission, Integer order, Boolean state) {
+        this.id = id;
+        this.board = board;
+        this.mission = mission;
+        this.order = order;
+        this.state = state;
+    }
+
+    public Tile() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Mission getMission() {
+        return mission;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void changeState(Boolean state){
+        this.state = state;
+    }
+
+    public void changeMission(Mission mission){
+        this.mission = mission;
+    }
+}
