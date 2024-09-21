4c49c93e1f69d97a6e939013e98be97b12d3693c
dkswoals <a4445656@gmail.com>
2024-09-21 16:59:25 +0900

    Feat: ErrorResponse 구현 및 전역 예외 헨들러 구현

---
+++ Server/src/main/java/com/example/server/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.server.common.exception;
+
+import com.example.server.dto.responseDTO.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getState().value(),
+            e.getMessage());
+        return ResponseEntity.status(e.getErrorCode().getState()).body(errorResponse);
+    }
+}
