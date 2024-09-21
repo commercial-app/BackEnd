d96e44964fe6e1c3758f81140e74b1e4d4b2dd43
dkswoals <a4445656@gmail.com>
2024-09-21 15:03:36 +0900

    Feat: MemberRepository 구현

---
+++ Server/src/main/java/com/example/server/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.server.repository;
+
+import com.example.server.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
