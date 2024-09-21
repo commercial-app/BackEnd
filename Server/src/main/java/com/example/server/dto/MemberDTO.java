5e65b0d595f2b00275faaba183d5057ee16eee08
dkswoals <a4445656@gmail.com>
2024-09-21 15:56:34 +0900

    Feat: MemberDTO toEntity 메서드 추가

--- Server/src/main/java/com/example/server/dto/MemberDTO.java
+++ Server/src/main/java/com/example/server/dto/MemberDTO.java
@@ -1,7 +1,12 @@
 package com.example.server.dto;
 
+import com.example.server.entity.Member;
 import com.example.server.entity.Role;
 
-public record MemberDTO(Long memberId, String email, String name, Long point, Role role) {
+public record MemberDTO(Long memberId, String email, String password, String name, Long point,
+                        Role role) {
 
+    public Member toEntity() {
+        return new Member(memberId, email, password, name, point, role);
+    }
 }
