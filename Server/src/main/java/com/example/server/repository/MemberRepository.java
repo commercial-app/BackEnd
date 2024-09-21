85fbd6bd25b11e90c00280fbf8c24c8600308508
dkswoals <a4445656@gmail.com>
2024-09-21 17:13:46 +0900

    Feat: MemberService existEmail, matchPassword 메서드 구현

--- Server/src/main/java/com/example/server/repository/MemberRepository.java
+++ Server/src/main/java/com/example/server/repository/MemberRepository.java
@@ -4,5 +4,5 @@ import com.example.server.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Boolean existsByEmail(String email);
 }
--- Server/src/main/java/com/example/server/service/MemberService.java
+++ Server/src/main/java/com/example/server/service/MemberService.java
@@ -1,10 +1,11 @@
 package com.example.server.service;
 
+import com.example.server.common.exception.CustomException;
+import com.example.server.common.exception.ErrorCode;
 import com.example.server.dto.MemberDTO;
 import com.example.server.entity.Member;
 import com.example.server.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +14,25 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public MemberDTO getMemberDTOByMemberId(Long memberId) throws NotFoundException {
-        return memberRepository.findById(memberId).orElseThrow(NotFoundException::new)
+    public MemberDTO getMemberDTOByMemberId(Long memberId) {
+        return memberRepository.findById(memberId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER))
             .toDTO();
     }
 
     public Long saveMember(MemberDTO memberDTO) {
         Member member = memberDTO.toEntity();
         return memberRepository.save(member).getMemberId();
+    }
+
+    public Boolean existEmail(String email) {
+        return memberRepository.existsByEmail(email);
+    }
+
+    public void matchPassword(MemberDTO memberDTO, String password) {
+        if (memberDTO.password().equals(password)) {
+            throw new CustomException(ErrorCode.PASSWORD_NOT_MATCH);
+        }
     }
 
 }
