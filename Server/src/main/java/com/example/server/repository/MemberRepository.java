87cc419b6af15db5b267fdb8ba46993fa4d69fb6
dkswoals <a4445656@gmail.com>
2024-09-21 19:19:26 +0900

    Feat: MemberRepository, MemberService 수정

--- Server/src/main/java/com/example/server/repository/MemberRepository.java
+++ Server/src/main/java/com/example/server/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.example.server.repository;
 
 import com.example.server.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
--- Server/src/main/java/com/example/server/service/MemberService.java
+++ Server/src/main/java/com/example/server/service/MemberService.java
@@ -7,6 +7,7 @@ import com.example.server.entity.Member;
 import com.example.server.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,17 +15,27 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     public MemberDTO getMemberDTOByMemberId(Long memberId) {
         return memberRepository.findById(memberId)
             .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER))
             .toDTO();
     }
 
+    @Transactional(readOnly = true)
+    public MemberDTO getMemberDTOByEmail(String email) {
+        return memberRepository.findByEmail(email)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER))
+            .toDTO();
+    }
+
+    @Transactional
     public Long saveMember(MemberDTO memberDTO) {
         Member member = memberDTO.toEntity();
         return memberRepository.save(member).getMemberId();
     }
 
+    @Transactional(readOnly = true)
     public void existEmail(String email) {
         if (memberRepository.existsByEmail(email)) {
             throw new CustomException(ErrorCode.DUPLICATE_EMAIL);
@@ -32,7 +43,7 @@ public class MemberService {
     }
 
     public void matchPassword(MemberDTO memberDTO, String password) {
-        if (memberDTO.password().equals(password)) {
+        if (!memberDTO.password().equals(password)) {
             throw new CustomException(ErrorCode.PASSWORD_NOT_MATCH);
         }
     }
