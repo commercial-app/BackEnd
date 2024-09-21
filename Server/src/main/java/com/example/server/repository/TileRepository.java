d04baf24943b1fe7aa4179704b76893718ae2a17
dkswoals <a4445656@gmail.com>
2024-09-22 02:24:28 +0900

    Feat: TileRepository findByMissionIdAndMemberId 메서드 추가

--- Server/src/main/java/com/example/server/repository/TileRepository.java
+++ Server/src/main/java/com/example/server/repository/TileRepository.java
@@ -2,10 +2,22 @@ package com.example.server.repository;
 
 import com.example.server.entity.Board;
 import com.example.server.entity.Tile;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TileRepository extends JpaRepository<Tile, Long> {
     List<Tile> findByBoard(Board board); // 보드 내 타일들을 찾기 위한 메서드
+
+    @Query("SELECT t FROM Tile t " +
+        "JOIN t.board b " +
+        "JOIN b.member m " +
+        "WHERE t.mission.missionId = :missionId " +
+        "AND m.memberId = :memberId")
+    Optional<Tile> findByMissionIdAndMemberId(@Param("missionId") Long missionId,
+        @Param("memberId") Long memberId);
+
 }
