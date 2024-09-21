f75f39456cf0b832d29284b363b5fd8613bf2070
dkswoals <a4445656@gmail.com>
2024-09-21 17:35:19 +0900

    Feat: 전역 헨들러 처리 예외 추가
    Jwt 관련 예외 처리 추가

--- Server/src/main/java/com/example/server/common/exception/GlobalExceptionHandler.java
+++ Server/src/main/java/com/example/server/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.server.common.exception;
 
 import com.example.server.dto.responseDTO.ErrorResponse;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = CustomException.class)
+    @ExceptionHandler(value = {CustomException.class, JwtException.class})
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getState().value(),
             e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getState()).body(errorResponse);
     }
+
+
 }
