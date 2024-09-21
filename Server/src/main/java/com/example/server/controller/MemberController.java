5074e8ebd13b2e2382930f66152e0e31d6ba6473
dkswoals <a4445656@gmail.com>
2024-09-21 19:19:46 +0900

    Feat: MemberController 구현

---
+++ Server/src/main/java/com/example/server/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.example.server.controller;
+
+import com.example.server.common.jwtUtil.JwtProvider;
+import com.example.server.dto.MemberDTO;
+import com.example.server.dto.requestDTO.LoginRequest;
+import com.example.server.dto.requestDTO.RegisterRequest;
+import com.example.server.dto.responseDTO.JwtResponse;
+import com.example.server.entity.Role;
+import com.example.server.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final JwtProvider jwtProvider;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        memberService.existEmail(request.email());
+        MemberDTO memberDTO = new MemberDTO(0L, request.email(), request.password(), request.name(),
+            0L, Role.USER);
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.saveMember(memberDTO));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequest request) {
+        MemberDTO memberDTO = memberService.getMemberDTOByEmail(request.email());
+        memberService.matchPassword(memberDTO, request.password());
+        String token = jwtProvider.generateToken(memberDTO.memberId());
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+}
