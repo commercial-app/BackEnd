5b2789b41c2c96b9db8117fa8e3218dd2d350467
dkswoals <a4445656@gmail.com>
2024-09-21 23:32:03 +0900

    Refactor: 카테고리 테이블명 수정

--- Server/src/main/java/com/example/server/entity/MissionCategory.java
+++ Server/src/main/java/com/example/server/entity/MissionCategory.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Table(name = "mission_categories")
 @Entity
 @Data
 @NoArgsConstructor
