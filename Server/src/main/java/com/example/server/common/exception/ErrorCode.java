2b994648968bd1a6225effee6e03dc215dc146f2
dkswoals <a4445656@gmail.com>
2024-09-21 16:58:44 +0900

    Feat: ErrorCode 열거형 구현

---
+++ Server/src/main/java/com/example/server/common/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.server.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND,"존재하지 않는 유저입니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST,"비밀번호가 일치하지 않습니다.");
+
+    private final HttpStatus state;
+    private final String message;
+
+}
