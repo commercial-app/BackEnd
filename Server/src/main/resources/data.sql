e043db87f88d9456a19ee9b5e32c92168f1def63
dkswoals <a4445656@gmail.com>
2024-09-22 16:29:24 +0900

    Chore: category 생성 SQL 추가

---
+++ Server/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- 미션 카테고리 생성
+INSERT INTO mission_categories (name) VALUES ('관광');
+INSERT INTO mission_categories (name) VALUES ('맛집');
+INSERT INTO mission_categories (name) VALUES ('카페');
+INSERT INTO mission_categories (name) VALUES ('문화');
+INSERT INTO mission_categories (name) VALUES ('자연');
